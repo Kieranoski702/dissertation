4a2f539ba311daf273c646bcf7cecf94bc513025
kieranoski <kfowlds1812@gmail.com>
2025-03-03 18:08:04 +0000

    Test 5

--- scripts/wiimote_to_emulator.py
+++ scripts/wiimote_to_emulator.py
@@ -156,9 +156,12 @@ def main():
                         )
 
                         # Normalize the raw values.
-                        norm_ax = (108 * -x_val) + 512
-                        norm_ay = (108 * z_val) + 512
-                        norm_az = (108 * -y_val) + 512
+                        # norm_ax = (108 * -x_val) + 512
+                        # norm_ay = (108 * z_val) + 512
+                        # norm_az = (108 * -y_val) + 512
+                        norm_ax = x_val
+                        norm_ay = y_val
+                        norm_az = z_val
                         logger.debug(
                             "Normalized accelerometer: ax=%.3f, ay=%.3f, az=%.3f",
                             norm_ax,
