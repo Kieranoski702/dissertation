de4a96410a08bf350bf9e4c164806d8760d1f549
kieranoski <kfowlds1812@gmail.com>
2025-03-04 16:59:29 +0000

    Use 527.5

--- scripts/wiimote_to_emulator.py
+++ scripts/wiimote_to_emulator.py
@@ -156,9 +156,9 @@ def main():
                         )
 
                         # Normalize the raw values.
-                        norm_ax = (x_val) + 527.0
-                        norm_ay = (y_val) + 527.0
-                        norm_az = (z_val) + 527.0
+                        norm_ax = (x_val) + 527.5
+                        norm_ay = (y_val) + 527.5
+                        norm_az = (z_val) + 527.5
                         logger.debug(
                             "Normalized accelerometer: ax=%.3f, ay=%.3f, az=%.3f",
                             norm_ax,
